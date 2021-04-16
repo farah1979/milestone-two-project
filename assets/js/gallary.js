7182ae377c56231f40f57d77e1a2f59249dc16ca
farah1979 <farahhisham1979@gmail.com>
2021-04-16 10:15:40 +0000

    initial new page answer.html

--- assets/js/gallary.js
+++ assets/js/gallary.js
@@ -1,57 +1,21 @@
 //                                                                Gallary-page
-gallarySelect("all") //---------------------Show All Items
-
-function gallarySelect(data) {
-  let i;
-  let x = $(".column");
-  if (data == "all") {
-    data = "";
-  };
-  
-  for (i = 0; i < x.length; i++) {
-    gallaryClassDelete(x[i], "show");
-    if (x[i].className.indexOf(data) > -1){ 
-      gallaryAddClass(x[i], "show");
-    };
-    
+onFilterSelect("all") //---------------------Show All Items
+function onFilterSelect(filterType) {
+  let galleryImages = $(".column");
+  for(var i=0; i < galleryImages.length; i++) {
+      if(galleryImages[i].className.indexOf(filterType) > -1 || filterType === 'all'){ 
+        $(galleryImages[i]).addClass("show");
+      } else {
+        $(galleryImages[i]).removeClass("show");
+      }
   };
 };
-
-
-function gallaryAddClass(item, name) {
-  var i;
-  var str1;
-  var  str2;
-  str1 = item.className.split(" ");
-  str2 = name.split(" ");
-  for (i = 0; i < str2.length; i++) {
-    if (str1.indexOf(str2[i]) == -1) {
-      item.className += " " + str2[i];
-    };
-  };
-};
-
-function gallaryClassDelete(item, name) {
-   var i;
-   var  str1;
-   var str2;
-  str1 = item.className.split(" ");
-  str2 = name.split(" ");
-  for (i = 0; i < str2.length; i++) {
-    while (str1.indexOf(str2[i]) > -1) {
-      str1.splice(str1.indexOf(str2[i]), 1);     
-    }
-  }
-  item.className = str1.join(" ");
-}
-
-
-var container = $("#myGallary");
-var b = Container.$(".btn");
-for (var i = 0; i < b.length; i++) {
-  b[i].addEventListener("click", function(){
+var galleryContainer = $("#myGallary");
+var buttons = Container.$(".btn");
+buttons.forEach(button => {
+  button.addEventListener("click", function(){
     var current = $(".active");
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
-  });
-}
+  });  
+});
